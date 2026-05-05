2a7a1b49ac2b15f6e261e24dd4b57158ad3b4017
zzzouv <166945024+zzzouv@users.noreply.github.com>
2026-05-05 17:24:20 -0400

    Delete bdget-main directory

--- bdget-main/src/main/java/com/example/bdget/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bdget.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.bdget.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long>{
-    
-}
